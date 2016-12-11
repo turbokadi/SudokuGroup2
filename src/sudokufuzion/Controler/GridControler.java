e3c12f6cf9bb9d8d430c5cdafc1c62f15bd14347
J4BB3R <johanmaurel@protonmail.com>
2016-12-11 22:07:04 +0100

    [View] Adding of KeyListener + Creation of GC Class Base

---
+++ src/sudokufuzion/Controler/GridControler.java
@@ -0,0 +1,26 @@
+/**
+ *
+ *   //-- Create by J4BB3R<johanmaurel@gmail.com> --//
+ *
+ *   [Initial Date] : 08/12/2016
+ *   [Last Date] : 09/12/2016
+ *   [Description] :
+ *      This class is the grid controler;
+ *   [Increments] :
+ *      - 09/12/2016 [v0.1] : Creation of the Class + main method;
+ *
+ **/
+
+package sudokufuzion.Controler;
+
+public class GridControler {
+    
+    public GridControler() {
+
+    }
+    
+    public void addToTasksQueue(int keyCode) {
+        // Something to do
+    }
+    
+}
