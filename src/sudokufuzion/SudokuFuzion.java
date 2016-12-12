40a6fd18e15316912c64b28f1782452d17c23f61
Nicolas <nico@localhost.localdomain>
2016-12-12 21:07:21 +0100

    Modif pour joh

--- src/sudokufuzion/SudokuFuzion.java
+++ src/sudokufuzion/SudokuFuzion.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package sudokufuzion;
-
+import sudokufuzion.Modele.Grid;
 /**
  *
  * @author jabber
@@ -15,7 +15,14 @@ public class SudokuFuzion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Grid mat = new Grid();
+        //mat.fillGridTemp();
+        for(int i=0; i<9 ; i++){
+            for(int j = 0; j<9; j++){
+                System.out.print("[" + mat.getCase(i, j) + "]");
+            }
+            System.out.println();
+        }
     }
     
 }
