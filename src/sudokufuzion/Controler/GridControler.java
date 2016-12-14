f3520ce5c26aa1712bda2929e9bfa200fa88f826
Jabber <jabber@linux.home>
2016-12-14 01:55:31 +0100

    Mass Comms

--- src/sudokufuzion/Controler/GridControler.java
+++ src/sudokufuzion/Controler/GridControler.java
@@ -3,13 +3,13 @@
  *   //-- Create by J4BB3R<johanmaurel@gmail.com> --//
  *
  *   [Initial Date] : 08/12/2016
- *   [Last Date] : 09/12/2016
+ *   [Last Date] : 12/12/2016
  *   [Description] :
  *      This class is the grid controler;
  *   [Increments] :
  *      - 09/12/2016 [v0.1] : Creation of the Class + main method;
  *      - 11/12/2016 [v0.2] : Adding Taskthread
- *      - 12/12/2016 [v0.3] : Adding focus movement gesture
+ *      - 12/12/2016 [v0.3] : Adding focus movement, value change and error gesture
  *
  **/
 
@@ -26,18 +26,17 @@ import sudokufuzion.Controler.GridEvents.ErrorEvent;
 import sudokufuzion.Controler.GridEvents.MoveFocusEvent;
 import sudokufuzion.Modele.Grid;
 
-public class GridControler extends Observable{
+public class GridControler extends Observable{ // This Class implements an Observer pattern with the View
     
-    // Thread Runnable Declaration
-    private final Runnable th = new Runnable() {
+    
+    private final Runnable th = new Runnable() { // Thread Runnable Declaration
         @Override
         public void run() {
-            setChanged();
-            notifyObservers(new ChangeValueEvent().putGridIntoArrayList(grid));
-            while (true) {
+            notifyObservers(new ChangeValueEvent().putGridIntoArrayList(grid)); // Initial Loading of the matrix 
+            while (true) { // Infinite Loop
                 try {
-                    if(queue.isEmpty()) synchronized (Th) { Th.wait(); }
-                    handleEvent(queue.poll());
+                    if(queue.isEmpty()) synchronized (Th) { Th.wait(); } // When the Tasks Queue is empty the Thread is stopped and wait for a resume signal
+                    handleEvent(queue.poll()); // It Take and Remove the task from the Queue and give it to an analyse method "handleEvent()" 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -49,25 +48,24 @@ public class GridControler extends Observable{
     // Instance Attributes //
     //====================//    
     
-    private Point focusedCase;
-    private ConcurrentLinkedQueue<KeyEvent> queue;
-    private final Thread Th = new Thread(th);
-    private final Grid grid = new Grid();
+    private Point focusedCase; // It's the Coordinates of the case who is focused on the GUI 
+    private ConcurrentLinkedQueue<KeyEvent> queue; // The task Queue
+    private final Thread Th = new Thread(th); // The Thread Declaration
+    private final Grid grid = new Grid(); // Model Grid Declaration + Instanciation
     
     public GridControler() {
-        this.queue = new ConcurrentLinkedQueue<>();
+        this.queue = new ConcurrentLinkedQueue<>(); 
     }
     
-    public void runControler() { Th.run(); }
+    public void runControler() { Th.run(); } // Method who start the Thread
     
-    public void addToTasksQueue(KeyEvent evt) {
+    public void addToTasksQueue(KeyEvent evt) { // Method who push tasks into the Queue
         try {
-            Th.sleep(1);
-            if(queue.isEmpty()) {
+            if(queue.isEmpty()) { // If the queue is Empty,  push your task and notify the Thread for a waiting Traitement
                 queue.offer(evt);
-                synchronized (Th) { Th.notify(); }
+                synchronized (Th) { Th.notify(); } // Equivalent to Thread.resume()
             } else {
-                queue.offer(evt);
+                queue.offer(evt); // The task is push into the Queue
             }       
         } catch (Exception e) {
             e.printStackTrace();
@@ -78,52 +76,57 @@ public class GridControler extends Observable{
         
         try {
             
-            int val = evt.getKeyCode();
+            int val = evt.getKeyCode(); // Get the value of the Pressed Key
             
-            if(val >= KeyEvent.VK_NUMPAD1 && val <= KeyEvent.VK_NUMPAD9) {
-                if (!grid.verifyInitialValue(focusedCase)) {
+            if (val == KeyEvent.VK_DELETE) { // If the Key is a Suppr, it send to view an clear order for the focusedCase coordinates
+                if (!grid.verifyInitialValue(focusedCase)) { // Verify if the case is an non modifiable grid case
+                    
+                    sendChangeValueEvent(focusedCase, 0); // send change zero to assign a null string into the grid case
+                    
+                }
+            }
+            else if(val >= KeyEvent.VK_NUMPAD1 && val <= KeyEvent.VK_NUMPAD9) { // Detect if is a pav num Key
+                if (!grid.verifyInitialValue(focusedCase)) { // Verify if the case is an non modifiable grid case 
                     
                     int keyValue = val - KeyEvent.VK_NUMPAD0; // Value of the key compare to 0 ID value
                     
-                    ChangeValueEvent ev = new ChangeValueEvent();
-                    ev.add(new Case(this.focusedCase.x, this.focusedCase.y, keyValue));
-                    this.setChanged();
-                    this.notifyObservers(ev);
+                    sendChangeValueEvent(focusedCase, keyValue); // send change val to assign the value into the grid case
                     
-                    ArrayList<Point> buff = grid.setCase(this.focusedCase.x, this.focusedCase.y, keyValue);
+                    ArrayList<Point> buff = grid.setCase(this.focusedCase.x, this.focusedCase.y, keyValue); // check errors
                     
-                    if(buff!=null) {
+                    if(buff!=null) { // if non null means the Case generate errors
                         
                         ErrorEvent ec = new ErrorEvent(buff);
-                        ec.setInitialCase((Point) focusedCase.clone());
-                        this.setChanged();
-                        this.notifyObservers(ec);
+                        ec.setInitialCase((Point) focusedCase.clone()); // Assign case who generate error 
+                        this.notifyObservers(ec); // notify view
+                        
+                    } else {
+                        
+                        if (grid.victory()) ; // On WORK
                         
                     }
+                    
                 }
             } 
-            else if(val >= KeyEvent.VK_LEFT && val <= KeyEvent.VK_DOWN){
+            else if(val >= KeyEvent.VK_LEFT && val <= KeyEvent.VK_DOWN){ // If the key is a movement Key UP, DOWN, LEFT, RIGHT
                               
                 switch (val) {
+                    
                     case KeyEvent.VK_UP : 
-                        this.setChanged();
-                        this.notifyObservers(new MoveFocusEvent().setMoveUp());
+                        this.notifyObservers(new MoveFocusEvent().setMoveUp()); // notify view with the correspondant move value
                     break;
                     case KeyEvent.VK_DOWN :
-                        this.setChanged();
-                        this.notifyObservers(new MoveFocusEvent().setMoveDown());
+                        this.notifyObservers(new MoveFocusEvent().setMoveDown()); // notify view with the correspondant move value
                     break;
                     case KeyEvent.VK_RIGHT :
-                        this.setChanged();
-                        this.notifyObservers(new MoveFocusEvent().setMoveRight());
+                        this.notifyObservers(new MoveFocusEvent().setMoveRight()); // notify view with the correspondant move value
                     break;
                     case KeyEvent.VK_LEFT :
-                        this.setChanged();
-                        this.notifyObservers(new MoveFocusEvent().setMoveLeft());
+                        this.notifyObservers(new MoveFocusEvent().setMoveLeft()); // notify view with the correspondant move value
                     break;
                     default :
-                }
-                
+                        
+                }     
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -131,5 +134,19 @@ public class GridControler extends Observable{
     }
     
     public void setFocusedCase(Point pt) { this.focusedCase = pt; }
+    
+    private void sendChangeValueEvent(Point pt, int value) {
+        
+        ChangeValueEvent ev = new ChangeValueEvent();
+        ev.add(new Case(pt.x, pt.y, value)); // Create an modication Event and add Case to modify
+        this.notifyObservers(ev); // notify view
+        
+    }
+    
+    @Override
+    public void notifyObservers(Object o1) { // Method who simplify notification process
+        this.setChanged();
+        super.notifyObservers(o1);
+    }
     
 }
--- src/sudokufuzion/Controler/GridEvents/MoveFocusEvent.java
+++ src/sudokufuzion/Controler/GridEvents/MoveFocusEvent.java
@@ -1,30 +1,42 @@
+/**
+ *
+ *   //-- Create by J4BB3R<johanmaurel@gmail.com> --//
+ *
+ *   [Initial Date] : 11/12/2016
+ *   [Last Date] : 11/12/2016
+ *   [Description] :
+ *      This class is an Modified ArrayList Typed to be detect as a Move Focus Event into the View;
+ *   [Increments] :
+ *      - 11/12/2016 [v0.1] : Creation of the Class
+ *
+ **/
 
 package sudokufuzion.Controler.GridEvents;
 
 import sudokufuzion.Views.mainView;
 
-public class MoveFocusEvent {
+public class MoveFocusEvent { // This class represent a Move by a value, Key Pressed by Key Pressed
     
-    private int Move = mainView.ERROR ;
+    private int Move = mainView.ERROR ; // This is the attribute who store the Move value, by default is an impossible move value to not impact the View in case of an error.
     
     public int getMove() { return Move; }
     
-    public MoveFocusEvent setMoveUp() { 
+    public MoveFocusEvent setMoveUp() { // Method who set the UP move value into the Attr and return it instance
         Move = mainView.UP;
         return this;
     }
     
-    public MoveFocusEvent setMoveDown() { 
+    public MoveFocusEvent setMoveDown() { // Method who set the DOWN move value into the Attr and return it instance
         Move = mainView.DOWN;
         return this;
     }
     
-    public MoveFocusEvent setMoveLeft() { 
+    public MoveFocusEvent setMoveLeft() { // Method who set the LEFT move value into the Attr and return it instance
         Move = mainView.LEFT;
         return this;
     }
     
-    public MoveFocusEvent setMoveRight() { 
+    public MoveFocusEvent setMoveRight() { // Method who set the RIGHT move value into the Attr and return it instance
         Move = mainView.RIGHT;
         return this;
     }
