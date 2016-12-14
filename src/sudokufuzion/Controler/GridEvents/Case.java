34f77bf45ead7c83e189f5bdc9f35a1c63b144d2
J4BB3R <johanmaurel@protonmail.com>
2016-12-14 01:55:31 +0100

    Mass Comms

--- src/sudokufuzion/Controler/GridEvents/Case.java
+++ src/sudokufuzion/Controler/GridEvents/Case.java
@@ -1,12 +1,24 @@
+/**
+ *
+ *   //-- Create by J4BB3R<johanmaurel@gmail.com> --//
+ *
+ *   [Initial Date] : 11/12/2016
+ *   [Last Date] : 11/12/2016
+ *   [Description] :
+ *      This class is an struct who contains value of a grid case
+ *   [Increments] :
+ *      - 11/12/2016 [v0.1] : Creation of the Class
+ *
+ **/
 
 package sudokufuzion.Controler.GridEvents;
 
 public class Case {
     
     public int x, y, value;
-    private boolean initial = false;
+    private boolean initial = false; // It's true if the case is an initial on modifiable case the sudoku
     
-    public Case(int x, int y, int value) {
+    public Case(int x, int y, int value) { 
         this.x = x;
         this.y = y;
         this.value = value;
--- src/sudokufuzion/Views/Components/GridMainPanel.java
+++ src/sudokufuzion/Views/Components/GridMainPanel.java
@@ -18,15 +18,15 @@ import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
-public class GridMainPanel extends JPanel{
+public class GridMainPanel extends JPanel{ // It's a JPanel modified to have the right comportement and attributes
 
     // Panel Configuration
-    private final static int PANEL_WIDTH = 94, PANEL_HEIGHT = 94;
+    private final static int PANEL_WIDTH = 94, PANEL_HEIGHT = 94; // Size of SubGrib
     
     // Border Configuration
-    public final static int BORDER_SIZE = 2;
-    public final static Color BORDER_COLOR = new Color(80,80,80,255);
-    private final static Border BORDER = (Border) new LineBorder(GridMainPanel.BORDER_COLOR, GridMainPanel.BORDER_SIZE);
+    public final static int BORDER_SIZE = 2; // Border size of the SubGrib, public to be use from the outside to excel offset position
+    private final static Color BORDER_COLOR = new Color(80,80,80,255); // Border color of the SubGrib, public to be use from the outside to excel offset position
+    private final static Border BORDER = (Border) new LineBorder(GridMainPanel.BORDER_COLOR, GridMainPanel.BORDER_SIZE); // Border Object of the SubGrib
     
     public GridMainPanel(){
         this.setSize(GridMainPanel.PANEL_WIDTH, GridMainPanel.PANEL_HEIGHT);
